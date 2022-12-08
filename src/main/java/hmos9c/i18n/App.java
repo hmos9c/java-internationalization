c3b951a5bb36d0c683e8049a12d31ca7323cb43c
hmos9c <hmos9c@gmail.com>
2022-12-08 14:31:26 +0700

    Membuat Project Java I18N

---
+++ src/main/java/hmos9c/i18n/App.java
@@ -0,0 +1,13 @@
+package hmos9c.i18n;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
